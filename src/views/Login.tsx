2e4100dcfb54ede60eaa0da2bee073017a5ecf83
leandro marrero <leandro.marrero03@gmail.com>
2023-11-22 22:49:00 -0300

    build

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,4 +1,4 @@
-import { redirect, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { useSnackbar } from "notistack"
 // Styles
 import '../styles/login.css'
