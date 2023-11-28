26a364badcc748c8fccdc9aff4ce08e831a5e567
FacundoBauza <31938804+FacundoBauza@users.noreply.github.com>
2023-11-28 15:15:01 -0300

    cambio de dominio

--- src/assets/constant.tsx
+++ src/assets/constant.tsx
@@ -1,2 +1,2 @@
 //export const BACKEND_URL: string = "https://localhost:32770/";
-export const BACKEND_URL: string = "https://104.131.5.69/";
+export const BACKEND_URL: string = "https://puertaverde.lat/";
